31fa2a424ca353ab932c7ddc6eb4b9d0b0d2a5b7
Juan Aquino <juanpatricioaquino@gmail.com>
2021-06-06 22:52:02 -0300

    Tercer examen completado

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const paginate = function (items, page, itemsPerPage) {
-    /* YOUR CODE HERE */
+
+    // Se define la cantidad de items en el arreglo
+    const totalItems = items.length;
+    // Se define la cantidad de paginas
+    const numberOfPages = totalItems / itemsPerPage;
+    // Se define el indice desde donde se recorre el arreglo
+    const startingIndex = (page - 1) * itemsPerPage;
+    // Se define el primer elemento a tener en cuenta en el arreglo
+    const from = (startingIndex + 1) === totalItems ? totalItems : (startingIndex + 1);
+    // Se define el ultimo elemento a tener en cuenta en el arreglo
+    const to = (startingIndex + itemsPerPage) >= totalItems ? totalItems : (startingIndex + itemsPerPage);
+    // Se define la lista de elementos a devolver (entre from y to)
+    const itemsOnPage = items.slice(from - 1, to);
+    // Se define si hay elementos previos a la pagina elegida
+    const prev = page > 1;
+    // Se define si hay elementos posteriores a la pagina elegida
+    const next = page < numberOfPages;
+
+    return { totalItems, itemsOnPage, page, itemsPerPage, from, to, next, prev };
+
 
 }
 
